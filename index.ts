1453fa2e72bbb96550f0ad13b81ce8993eebdf56
益生菌 <yskun@outlook.com>
2017-07-08 16:17:37 +0800

    添加index文件

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-
+export * from './src/bin/express-server';
+export * from './src/bin/router-handler';
+export * from './src/bin/router/response.handler';
+export * from './src/decoration/res-message.interface';
+export * from './src/decoration/symbol';
 
 /**
  * Created by yskun on 2017/7/3.
