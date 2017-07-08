2a07f17c8ba1bb25dcaf63b97da35c87d7b2c507
益生菌 <yskun@outlook.com>
2017-07-08 20:52:24 +0800

    bug fix #1

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 export * from './src/bin/express-server';
 export * from './src/bin/router-handler';
 export * from './src/bin/router/response.handler';
+
+export * from './src/decoration/express'
 export * from './src/decoration/res-message.interface';
 export * from './src/decoration/symbol';
 
+export * from './src/define/express-options.interface';
 /**
  * Created by yskun on 2017/7/3.
  */
